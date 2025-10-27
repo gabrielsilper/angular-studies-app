e1e20a816370b63937bbef04778e0464d246cc2e
gabrielsilper <gabrielsilper.main@gmail.com>
2025-10-26 21:00:27 -0400

    wip: trying request some api on Angular

--- src/app/anime-quotes/anime-quotes.ts
+++ src/app/anime-quotes/anime-quotes.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import { QuoteCard } from "./quote-card/quote-card";
+import { Component, OnInit } from '@angular/core';
+import { QuoteCard } from './quote-card/quote-card';
+import { Quote } from './models/Quote';
+import { QuoteService } from './services/QuoteService';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-anime-quotes',
-  imports: [QuoteCard],
+  imports: [QuoteCard, HttpClientModule],
   templateUrl: './anime-quotes.html',
-  styleUrl: './anime-quotes.css'
+  styleUrl: './anime-quotes.css',
 })
-export class AnimeQuotes {
+export class AnimeQuotes implements OnInit {
+  public quote!: Quote;
+  public isLoading = true;
 
+  constructor(private quoteService: QuoteService) {}
+
+  async ngOnInit(): Promise<void> {
+    const quoteResponse = await this.quoteService.getQuote()
+
+    console.log(quoteResponse);
+    
+    this.isLoading = false;
+  }
 }
--- src/app/anime-quotes/models/Quote.ts
+++ src/app/anime-quotes/models/Quote.ts
@@ -1,14 +1,3 @@
-type Anime = {
-  id: number;
-  name: string;
-  altName: string;
-};
-
-type Character = {
-  id: number;
-  name: string;
-};
-
 export class Quote {
-  constructor(private content: string, private anime: Anime, private character: Character) {}
+  constructor(public q: string, public a: string, public h: string) {}
 }
