0b0c6c635801d9a5b7019a82363ea82ad9ec3aa8
gabrielsilper <gabrielsilper.main@gmail.com>
2025-10-30 12:09:00 -0400

    feat: add form group and controls to handle the user form

--- src/app/users/create-user-form/create-user-form.ts
+++ src/app/users/create-user-form/create-user-form.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'create-user-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-user-form.html',
-  styleUrl: './create-user-form.css'
+  styleUrl: './create-user-form.css',
 })
 export class CreateUserForm {
+  userForm = new FormGroup({
+    nameField: new FormControl(''),
+    emailField: new FormControl(''),
+    passwordField: new FormControl(''),
+    addressField: new FormControl(''),
+  });
 
+  submitForm() {
+    console.log(this.userForm.value);
+  }
 }
