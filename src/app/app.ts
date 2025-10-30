cae93de1fafaaa1461111c850b2b3b66edd86e14
gabrielsilper <gabrielsilper.main@gmail.com>
2025-10-30 07:30:39 -0400

    feat: create a new "application" for users and practice forms

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { WishModule } from './wish/wish-module';
 import { MovieModule } from './movie/movie-module';
+import { Users } from "./users/users";
+import { UsersModule } from './users/users-module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [WishModule,MovieModule],
+  imports: [WishModule, MovieModule, UsersModule],
 })
 export class App {}
