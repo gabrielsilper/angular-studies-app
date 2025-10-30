e72b708c1b4ef16246cb98a96cb59bfcfbfc13bc
gabrielsilper <gabrielsilper.main@gmail.com>
2025-10-29 20:53:48 -0400

    feat: implements search movie fuction and remove debug borders

--- src/app/movie/movie.ts
+++ src/app/movie/movie.ts
@@ -3,9 +3,10 @@ import { MovieService } from './services/movie-service';
 import { MovieResponse } from './models/MovieResponse';
 import { MovieDescription } from './models/MovieDescription';
 import { FormsModule } from '@angular/forms';
-import { Loading } from "./loading/loading";
+import { Loading } from './loading/loading';
 import { MoviesSection } from './movies-section/movies-section';
-import { SearchMovieForm } from "./search-movie-form/search-movie-form";
+import { SearchMovieForm } from './search-movie-form/search-movie-form';
+import { MovieEventService } from './services/movie-event-service';
 
 @Component({
   selector: 'app-movie',
@@ -19,7 +20,11 @@ export class Movie implements OnInit {
 
   descriptions!: MovieDescription[];
 
-  constructor(private movieService: MovieService) {}
+  constructor(private movieService: MovieService, private movieEvent: MovieEventService) {
+    this.movieEvent.listen('search-movie', (event) => {
+      this.searchMovie(event)
+    });
+  }
 
   ngOnInit() {
     this.movieService
@@ -30,14 +35,11 @@ export class Movie implements OnInit {
       });
   }
 
-  searchMovie() {
+  searchMovie(movie: string) {
     this.isLoading = true;
-    this.movieService
-      .getMovieDescriptions(this.searchText)
-      .subscribe((movieResponse: MovieResponse) => {
-        this.descriptions = movieResponse.description;
-        this.isLoading = false;
-        this.searchText = '';
-      });
+    this.movieService.getMovieDescriptions(movie).subscribe((movieResponse: MovieResponse) => {
+      this.descriptions = movieResponse.description;
+      this.isLoading = false;
+    });
   }
 }
--- src/app/movie/search-movie-form/search-movie-form.ts
+++ src/app/movie/search-movie-form/search-movie-form.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { MovieEventService } from '../services/movie-event-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'search-movie-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './search-movie-form.html',
-  styleUrl: './search-movie-form.css'
+  styleUrl: './search-movie-form.css',
 })
 export class SearchMovieForm {
+  searchTextMovie = '';
 
+  constructor(private movieEvent: MovieEventService) {}
+
+  searchMovie() {
+    this.movieEvent.publish({
+      eventName: 'search-movie',
+      payload: this.searchTextMovie,
+    });
+    this.searchTextMovie = '';
+  }
 }
